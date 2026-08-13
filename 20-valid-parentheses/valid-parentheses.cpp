b438aa1fb72b842410406572987d3adf9f734eb5
MD ZAFAR <jafarimam775@gmail.com>
2026-08-13 13:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int siz = s.size();
+        stack<char>st;
+        for(int i=0; i<siz; i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch = st.top();
+                st.pop();
+                if(s[i] == ')' && ch == '(' || s[i] == ']' && ch == '[' || s[i] == '}' && ch == '{');
+                else return false;
+            }
+        }
+        return st.empty();
+
+    }
+};
