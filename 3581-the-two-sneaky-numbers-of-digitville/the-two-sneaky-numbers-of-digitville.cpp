68cc15a7f1969d933611c82e04438de1284dae20
MD ZAFAR <jafarimam775@gmail.com>
2026-08-21 01:00:07 +0530

    Time: 7 ms (8.47%) | Memory: 26.2 MB (50.28%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x : nums){
+            mp[x]++;
+        }
+        vector<int> ans;
+        for(auto it : mp){
+            if(it.second >= 2){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
